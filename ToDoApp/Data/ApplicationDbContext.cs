d0e1d03d0842237bcfb3c492e94ec5c6465a24b0
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-07-13 12:49:59 +0200

    Adding ToDo Controller and CRUD Views

--- ToDoApp/Data/ApplicationDbContext.cs
+++ ToDoApp/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
 
 namespace ToDoApp.Data
 {
@@ -12,5 +13,6 @@ namespace ToDoApp.Data
             : base(options)
         {
         }
+        public DbSet<ToDoApp.Models.ToDo> ToDo { get; set; }
     }
 }
---
+++ ToDoApp/Data/Migrations/20180713104222_ToDo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoApp.Data.Migrations
+{
+    public partial class ToDo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Details = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    IsDone = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
